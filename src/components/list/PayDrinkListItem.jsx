a5712845c3398c61e9cc667e18eeb2756711d487
kimgeonsu <kundoo0412@gmail.com>
2023-06-27 11:49:40 +0900

    ; 이거 빠져있길래 추가함

--- src/components/list/PayDrinkListItem.jsx
+++ src/components/list/PayDrinkListItem.jsx
@@ -4,25 +4,25 @@ import { ReactComponent as Espresso } from '../../asset/espresso.svg';
 import { ReactComponent as DeleteSvg } from '../../asset/delete.svg'
 
 const Wrapper = styled.div`
-  padding:8px;
-  margin-left: 64px;
-  width: 702px;
-  height: 144px;
-  margin-top: 8px;
-  border: 1px solid #000000;
-  display: flex;
-  align-items: center;
-  gap : 0px 132px;
+    padding:8px;
+    margin-left: 64px;
+    width: 702px;
+    height: 144px;
+    margin-top: 8px;
+    border: 1px solid #000000;
+    display: flex;
+    align-items: center;
+    gap : 0px 132px;
 `;
 
-const NameContainer=styled.div`
+const NameContainer = styled.div`
     display:flex;
     align-items: center;
     gap: 16px;
 `;
 
-const Image=styled.text`
-    width: 120.46540832519531px
+const Image = styled.text`
+    width: 120.46540832519531px;
     height: 122px
 `;
 
@@ -37,35 +37,35 @@ text-align: left;
 
 `;
 
-const ButtonContainer=styled.div`
- 
+const ButtonContainer = styled.div`
+
 `;
 
-const DeleteButton=styled.button`
-background: none;
-border: none;
-cursor: pointer;
+const DeleteButton = styled.button`
+    background: none;
+    border: none;
+    cursor: pointer;
 `;
 
-function PayDrinkListItem(props){
+function PayDrinkListItem(props) {
 
-return(
-    <Wrapper>
-        <NameContainer>
-        <Image><Espresso/></Image>
-     <Text>아메리카노</Text>
-     </NameContainer>
-    <ButtonContainer>
-     <Text>1개</Text>
-     
-     </ButtonContainer>
-     <NameContainer>    
-     <Text>4,000원</Text>
-     <DeleteButton>
-          <DeleteSvg />
-        </DeleteButton>
-     </NameContainer>
-    </Wrapper>
+    return (
+        <Wrapper>
+            <NameContainer>
+                <Image><Espresso /></Image>
+                <Text>아메리카노</Text>
+            </NameContainer>
+            <ButtonContainer>
+                <Text>1개</Text>
+
+            </ButtonContainer>
+            <NameContainer>
+                <Text>4,000원</Text>
+                <DeleteButton>
+                    <DeleteSvg />
+                </DeleteButton>
+            </NameContainer>
+        </Wrapper>
     );
 
 
