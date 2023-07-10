ac54c0aa3915dd031358f1bc91de3211e37a1ab9
kimgeonsu <kundoo0412@gmail.com>
2023-07-11 00:19:52 +0900

    말해!!!!

--- src/components/SpeechToText.jsx
+++ src/components/SpeechToText.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-// import { io } from 'socket.io-client';
+
 
 const SpeechToText = ({ children }) => {
   const {
@@ -10,31 +10,45 @@ const SpeechToText = ({ children }) => {
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
   const timeoutRef = useRef(null);
+  const [isOk, setIsOk] = useState(true);
 
   useEffect(() => {
     if (!browserSupportsSpeechRecognition) {
       return <span>Browser doesn't support speech recognition.</span>;
     }
-
     SpeechRecognition.startListening({ continuous: true, language: 'ko' })
   }, []);
 
-
   useEffect(() => {
     clearTimeout(timeoutRef.current);
-
     // 말이 끝났을 때
     timeoutRef.current = setTimeout(() => {
       if (transcript.length > 0) {
         console.log("2 seconds");
-        // const socket = io('http://localhost:5001');
-        // socket.on('speechtotext', (data) => {
-        //   console.log("speechtotext", data);
-        // })
-        axios.post('http://localhost:5001/kiosk/order', { data: transcript })
-          .then((response) => {
-            console.log(response)
-          })
+
+        if (isOk) {
+          setIsOk(false);
+          axios.post('http://localhost:5001/kiosk/order', { data: transcript })
+            .then((response) => {
+              setIsOk(true);
+              console.log(response['data']);
+              if (response['data']['type'] == 'recommend') {
+                const decodedData = atob(response['data']['data']);
+                const byteArray = new Uint8Array(decodedData.length);
+                for (let i = 0; i < decodedData.length; i++) {
+                  byteArray[i] = decodedData.charCodeAt(i);
+                }
+                const blob = new Blob([byteArray], { type: 'audio/mp3' });
+                const audio = new Audio();
+                audio.src = URL.createObjectURL(blob);
+                audio.muted = true;
+                audio.play();
+                // audio.muted = false;
+              } else {
+
+              }
+            })
+        }
       }
 
       resetTranscript();
@@ -47,6 +61,7 @@ const SpeechToText = ({ children }) => {
 
   return (
     <>
+      <button>dd</button>
       {children}
     </>
   );
